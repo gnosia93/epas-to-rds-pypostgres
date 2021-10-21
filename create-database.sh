02bbb1dd1d55334b684b67fa19ccea5f11bcf3b4
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2021-10-22 01:17:43 +0900

    Update create-database.sh

--- create-database.sh
+++ create-database.sh
@@ -6,5 +6,14 @@ PPAS_IP=`aws ec2 describe-instances --region=ap-northeast-2 \
     --output=text`
 PPAS_PORT=5444
 
+RDS_IP=`aws rds describe-db-instances --region=ap-northeast-2 \
+    --db-instance-identifier tf-postgres-rds \
+    --query "DBInstances[].Endpoint.Address" \
+    --output=text`
+RDS_PORT=5432
+
 echo 'ppas ip '$PPAS_IP
 psql -U enterprisedb -d edb -h $PPAS_IP -p $PPAS_PORT -a -f ./ppas-database.sql
+
+echo 'rds endpoint '$RDS_IP
+psql -U postgres -d postgres -h $RDS_IP -p $RDS_PORT -a -f ./ppas-database.sql
