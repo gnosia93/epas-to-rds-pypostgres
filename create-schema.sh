93a9a267baf171926c92a96f468ef8dacc1c5744
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2021-10-21 21:01:43 +0900

    Update create-schema.sh

--- create-schema.sh
+++ create-schema.sh
@@ -6,8 +6,8 @@ PPAS_IP=`aws ec2 describe-instances --region=ap-northeast-2 \
     --output=text`
 PPAS_PORT=5444
 
-echo $PPAS_IP
-psql -U enterprisedb -d edb -h $PPAS_IP -p $PPAS_PORT -a -f ./ppas-schema.sql
+echo 'ppas ip '$PPAS_IP
+psql -U shop -d shop_db -h $PPAS_IP -p $PPAS_PORT -a -f ./ppas-schema.sql
 
 
 #cp config.ini.ec2 config.ini
